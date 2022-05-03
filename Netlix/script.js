ba12f430aa9d62bee5e70b94a9cecb68070b982d
Kaique Zuba <84941614+zuba-px@users.noreply.github.com>
2022-05-03 15:36:11 -0300

    Update script.js

--- Netlix/script.js
+++ Netlix/script.js
@@ -2,7 +2,7 @@ function funcao1() {
     let email = document.getElementById('mail').value;
     let senha1 = document.getElementById('senha').value;
     let usuario = "email@gamour.com";
-    let usuarioSenha = "rdik37sa";
+    let usuarioSenha = "senha123";
     if (email == usuario & senha1 == usuarioSenha) {
         window.location.href = "file:///C:/Users/kaiqu/OneDrive/Área%20de%20Trabalho/Netlix/pagina.html";
     } else if (email == "" & senha1 == "") {
